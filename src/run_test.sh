ad592eece84019f4897e9826345797362388c512
Mingxiang Tuo <595983351@qq.com>
2019-04-09 21:49:24 +0800

    add sh and json and ipynb in .gitgnore

--- src/run_test.sh
+++
@@ -1,3 +0,0 @@
-python test.py -m zhidao/BiDAF/model_best_Rouge_67.29.pth -d 0 -t ./result/predict/zhidao_result.json
-python test.py -m search/BiDAF/model_best_Rouge_58.28.pth -d 0 -t ./result/predict/search_result.json
-python combine_result.py -z ./result/predict/zhidao_result.json -s ./result/predict/search_result.json -t ./result/predict/result.json
