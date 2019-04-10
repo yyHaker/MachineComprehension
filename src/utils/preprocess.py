3fe3997c98400e63531c7eeae844a26ffff46613
MrTuo <595983351@qq.com>
2019-04-10 09:08:42 +0800

    删除search 单para的处理函数，在多para处理中加入长度截取

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -22,7 +22,6 @@ def find_fake_answer_from_multi_paras(sample, paragraphs):
     best_e_idx = 0
     best_score = 0.
     best_para_idx = -1
-    best_para = []  # not return
     for para_idx, para in enumerate(paragraphs):
         fake_answer, s_idx, e_idx, score = find_fake_answer_2(sample, para)
         if score > best_score:
@@ -30,7 +29,6 @@ def find_fake_answer_from_multi_paras(sample, paragraphs):
             best_fake_answer = fake_answer
             best_s_idx = s_idx
             best_e_idx = e_idx
-            best_para = para
             best_para_idx = para_idx
     # print("answer match score: ", best_score)
     # make sure answer span is in one of the para and match score is not zero
@@ -38,86 +36,7 @@ def find_fake_answer_from_multi_paras(sample, paragraphs):
     return best_fake_answer, best_s_idx, best_e_idx, best_score, best_para_idx
 
 
-def find_search_paras(sample, train=True):
-    """ find search paragraph
-    1. 选择is_select=True的前3篇doc，每个doc取前10个para
-    2. 将标题和各段中间插入连接符号<sep>拼接在一起，没有超过最大长度（500）则返回这个段落，否则执行3
-    3. 对于每个doc中的10个段落，计算各段落和问题的BLEU-4分数，来衡量段落和问题的相关性
-    4. 在排名前3的段落中找到idx最小的段落（越靠前越有用），然后将该段落和该段落的下一段落拼接（下一段落可能包含答案）
-    5. 在剩余8个段落中，每段选取第一句话
-    6. 将上述所有内容拼接后，截取最大长度500返回
-    :param sample:
-    :return:
-    """
-    def first_sentence(para):
-        if not len(para):
-            return []
-        split_tag = ['。', '!', '?', '！', '？']
-        s = []
-        for word in para:
-            s.append(word)
-            if word in split_tag:
-                break
-        if s[-1] not in split_tag:
-            s.append('。')
-        return s
-
-    best_para = []
-    # 选择前三个document
-    docs = []
-    for doc in sample["documents"]:
-        if len(doc["segmented_paragraphs"]) != 0:
-            docs.append(doc)
-    docs = docs[:3]
-    for doc in docs:
-        paras = []
-        for para in doc["segmented_paragraphs"]:
-            if len(para) != 0:
-                paras.append(para)
-        paras = paras[: 10]
-        best_para += doc["segmented_title"]
-        best_para += ['<sep>']
-        for para in paras:
-            best_para += para
-    # 看全部拼接后长度是否超过阈值
-    if len(best_para) <= 500:
-        return best_para
-    else:
-        best_para = []
-        question = sample['segmented_question']
-        for doc in docs:
-            # 拼title
-            best_para += doc["segmented_title"]
-            best_para += ['<sep>']
-            # 仅选取前10个段落
-            paras = doc["segmented_paragraphs"][:10]
-            if paras:
-                # 计算Recall
-                prf_scores = [precision_recall_f1(para, question) for para in paras]
-                scores = [i[1] for i in prf_scores]
-                # 选取排名前2中最早出现的段落和下一段落
-                scores_idx = [(i, scores[i]) for i in range(len(scores))]
-                sorted_idx = sorted(scores_idx, key=lambda x: x[1], reverse=True)
-                choose_idx = [i[0] for i in sorted_idx[:2]]
-                # 拼接排名前2中最早出现的段落和下一段落
-                early_idx = min(choose_idx)
-                best_para += paras[early_idx]
-                early_next_idx = early_idx + 1
-                if early_next_idx < len(paras):
-                    best_para += paras[early_next_idx]
-                # 拼剩余段落的第一句话
-                for i in sorted_idx:
-                    best_para += first_sentence(paras[i[0]])
-                    if len(best_para) > 500:
-                        break
-                if len(best_para) > 500:
-                    break
-        # 截取最大长度500
-        best_para = best_para[:500]
-        return best_para
-
-
-def find_search_multi_paras(sample):
+def find_search_paras(sample):
     """ find search paragraph
     1. 选择is_select=True的前3篇doc，每个doc取前10个para
     2. 将标题和各段中间插入连接符号<sep>拼接在一起，没有超过最大长度（500）则返回这个段落，否则执行3
@@ -174,6 +93,7 @@ def find_search_multi_paras(sample):
             # 拼接段落
             for para in paras:
                 preprocess_para += para + ['<sep>']
+            preprocess_para = preprocess_para[:500]
             preprocess_docs.append(preprocess_para)
         return preprocess_docs
     else:
@@ -208,6 +128,7 @@ def find_search_multi_paras(sample):
                 for i in sorted_idx:
                     if i != early_idx and i != early_next_idx:
                         preprocess_para += first_sentence(paras[i[0]]) + ['<sep>']
+                preprocess_para = preprocess_para[:500]
                 preprocess_docs.append(preprocess_para)
         return preprocess_docs
 
